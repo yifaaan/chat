d8ea492b3a25034b6dbfc52dba52d6945ac517f3
yifaaan <0219lyf@gmail.com>
2024-09-17 12:07:17 +0800

    add sse server

---
+++ notify_server/src/sse.rs
@@ -0,0 +1,22 @@
+use std::{convert::Infallible, time::Duration};
+
+use axum::response::{sse::Event, Sse};
+use axum_extra::{headers, TypedHeader};
+use futures::{stream, Stream};
+use tokio_stream::StreamExt;
+
+pub(crate) async fn sse_handler(
+    TypedHeader(user_agent): TypedHeader<headers::UserAgent>,
+) -> Sse<impl Stream<Item = Result<Event, Infallible>>> {
+    println!("`{}` connected", user_agent.as_str());
+
+    let stream = stream::repeat_with(|| Event::default().data("hi!"))
+        .map(Ok)
+        .throttle(Duration::from_secs(1));
+
+    Sse::new(stream).keep_alive(
+        axum::response::sse::KeepAlive::new()
+            .interval(Duration::from_secs(1))
+            .text("keep-alive-text"),
+    )
+}
